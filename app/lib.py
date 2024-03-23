0cd254c8e216b0ae9593527d54ef133193821fd1
Joshua Hegedus <josh.hegedus@outlook.com>
2024-03-23 23:38:34 +0100

    added  lib fixes

--- app/lib.py
+++ app/lib.py
@@ -2,6 +2,8 @@ import os
 from fastapi import FastAPI
 from pyrepositories import DataSource
 from crud import CRUDApi
+from .models import Event
+
 
 def init_project():
     """Initializes the project by creating the data directory if it doesn't exist."""
@@ -15,7 +17,7 @@ def init_project():
     return data_dir
 
 
-def bootstrap(data_source: DataSource) -> tuple[CRUDApi, FastAPI]:
+def bootstrap(data_source: DataSource) -> CRUDApi:
     app = FastAPI(
         title="My API",
         description="This is a very fancy project, with auto docs for the API and everything.",
@@ -31,4 +33,35 @@ def bootstrap(data_source: DataSource) -> tuple[CRUDApi, FastAPI]:
         }
     )
 
-    return CRUDApi(data_source, app), app
+    return CRUDApi(data_source, app)
+
+def get_joiner_by_name(events, name):
+    for event in events:
+        for joiner in event.joiners or []:
+            if joiner.name == name:
+                return joiner
+
+    return None
+
+
+class EventAnalyzer:
+    def get_joiners_multiple_meetings(self, events: list[Event]) -> list[dict]:
+        """Returns a list of joiners, who have joined at least two meetings."""
+        joiners = {}
+        for event in events:
+            for joiner in event.joiners or []:
+                name = joiner.name
+                if name in joiners:
+                    joiners[name] += 1
+                else:
+                    joiners[name] = 1
+
+        j_list = []
+        for event in events:
+            for joiner in event.joiners or []:
+                name = joiner.name
+                if joiners[name] >= 2:
+                    j_list.append(get_joiner_by_name(events, name))
+
+        return j_list
+
